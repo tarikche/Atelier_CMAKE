947af656baf533c649902c067692d4675317a1ec
Tarik <114927678+tarikche@users.noreply.github.com>
2024-05-13 08:45:00 +0000

    test

--- src/main.c
+++ src/main.c
@@ -29,6 +29,7 @@ int main(int argc, char *argv[]){
 	else if (strcmp(op, "sqr") == 0) {
 		r = _mul(atof(a), atof(a));
 		printf("%lf",r);
+		printf("hdhdhdhdhdhdh");
 	}
 	else {printf("Erreur de parametres");}
 	}
